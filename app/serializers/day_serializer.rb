4b8afcb8741f6bb2a4042f2ede38909d44619c19
allansideas <allansideas@gmail.com>
2013-05-22 17:28:20 +0200

    hide back on first day created

--- app/serializers/day_serializer.rb
+++ app/serializers/day_serializer.rb
@@ -1,9 +1,13 @@
 class DaySerializer < ActiveModel::Serializer
-  attributes :id, :date, :pretty_date
+  attributes :id, :date, :pretty_date, :first_day_created
   has_many :metric_scores
   has_one :user
 
   def pretty_date
     object.date.strftime("%a #{object.date.day.ordinalize}")
+  end
+
+  def first_day_created
+    object.user.days.first.date == object.date
   end
 end
